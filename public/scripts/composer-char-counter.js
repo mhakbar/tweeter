51af1f8181aa3b8424c705e46e7400fb58fbbe95
mhakbar <habibakbar@live.com>
2022-02-02 14:46:48 -0800

    character counter for tweet implemented and working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+
+
+
+  $('textarea').on("input", function() {
+
+    // display correct number in counter
+    // get the right number from something
+    const characters = 140 - $(this).val().length;
+    const counter = $('.counter')[0];
+
+    if (characters < 0) {
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "black");
+
+    }
+
+    console.log(counter);
+
+
+    
+    counter.innerText = characters;
+    
+    
+    
+    // apply that number
+
+  });
+  
+});
+
+// https://api.jquery.com/on/
+
