cdd7eb96d8eaf10b6bf51078a5b64823f90f3ee4
mhakbar <habibakbar@live.com>
2022-02-03 16:03:46 -0800

    dynamic tweets functionality added with timer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 $(document).ready(() => {
-  const $inputButton = $('#tweetButton');
-  $inputButton.on('click', () => {
-    console.log('I got clicked!!!');
-    const $tweetMessage = $('.tweet-text');
 
-    const tweetOld = $tweetMessage.val();
 
-    const $li = $('<header>').text(tweetOld);
+  const createTweetElement = (tweet) => {
+    
+  
 
-    $('.tweetOld').append($li); 
+  const $tweet = $(`<article class="tweetOutline">Hello world</article>`);
+  $tweet.append(`<header class="tweetHeader">
+  <div class="nameAvatar">
+  <img src=${tweet.user.avatars}/>
+  <span id="name">${tweet.user.name}</span>
+</div>
+  <span>${tweet.user.handle}</span>
+</header>
+<p>${tweet.content.text}</p>
+<footer class="tweetFooter">
+  <h4>${timeago.format(tweet.created_at)}</h4>
+  <span class="tweetIcons">
+    <i class="fa fa-flag" idtweetObjg"></i>
+    <i class="fa fa-retweet" id="retweet"></i>             
+    <i class="fa fa-heart" id="heart"></i>
+  </span>
+</footer>`)
+  return $tweet;
+};
 
-    $tweetMessage.val('').focus();
 
-});
+// const $tweet = createTweetElement(tweetData);
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elem
+// $('#tweetContainer').append($tweet);
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  tweets.forEach((tweet) => {
+    const tweetElement = createTweetElement(tweet);
+    $('#tweetContainer').append(tweetElement);
+
+  })
+}
+renderTweets(data);
+
 })
+
+
