d963b51473e515806b16007c8ee56f43da39dcab
mhakbar <habibakbar@live.com>
2022-02-03 20:33:22 -0800

    Empty and Tweets over 140 characters cannot post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,30 +9,31 @@
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
+
 $(document).ready(() => {
 
 
@@ -82,16 +83,26 @@ const renderTweets = function(tweets) {
 // $(() => {
   const $inputButton = $('#submit-tweet');
   
+
+    
+  
   
   $('#submit-tweet').on('submit', () => {
     event.preventDefault();
     //console.log("random text");
+    const tweetPost = document.forms[0][0].value;
+
+  if (tweetPost.length > 140) {
+    alert("Exceeding");
+  } else if (!tweetPost) {
+    alert("empty");
+  }else {
     const serial = $('#submit-tweet').serialize();
     console.log(serial);
 
     $.post ("/tweets", serial, ).then(loadTweets);
 
-    
+    }
     
     //renderTweets(data);
   })
